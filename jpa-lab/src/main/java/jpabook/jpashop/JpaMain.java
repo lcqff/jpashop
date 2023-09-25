70997e7d7fac81c618b4722f95f16a9eb38ffffe
lcqff <yeonnu82g@gmail.com>
2023-09-25 19:11:38 +0900

    필요없는 코드 삭제

--- jpa-lab/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpa-lab/src/main/java/jpabook/jpashop/JpaMain.java
@@ -3,7 +3,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import jpabook.jpashop.domain.Member;
 
 public class JpaMain {
 
@@ -14,8 +13,6 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Member member = new Member();
-      member.se
       //code
       tx.commit();
     } catch (Exception e) {

b551aeb70742453ee600e931f98c9e455cfee53e
lcqff <yeonnu82g@gmail.com>
2023-09-26 21:46:39 +0900

    기본 문법과 쿼리 API - TypeQuery, Query - 결과 조회 api - 파라미터 바인딩

--- jpa-lab/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpa-lab/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import jpabook.jpashop.domain.Member;
 
 public class JpaMain {
 
@@ -13,7 +17,28 @@ public class JpaMain {
     tx.begin();
 
     try {
-      //code
+      Member member = new Member();
+      member.setUsername("memberA");
+      member.setAge(20);
+      em.persist(member);
+
+      //TypeQuery, Query
+      TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class) ;
+      Query query2 = em.createQuery("select m.username, m.age from Member m") ;
+
+      //결과 조회 api
+      TypedQuery<Member> singleResQuery = em.createQuery("select m from Member m where m.id = 1", Member.class);
+      List<Member> members = query.getResultList();
+      Member singleMember = singleResQuery.getSingleResult();
+
+      //파라미터 바인딩
+      Member member1 =
+          em.createQuery("select m from Member m where m.username = :username", Member.class)
+              .setParameter("username","memberA")
+              .getSingleResult();
+      System.out.println("파라미터 바인딩:" + member1.getUsername());
+
+
       tx.commit();
     } catch (Exception e) {
       tx.rollback();

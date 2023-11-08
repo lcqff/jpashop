ba0b8843d23365da958e90c34ac617e51c8d1f73
lcqff <yeonnu82g@gmail.com>
2023-11-08 21:33:53 +0900

    섹션2: 엔티티 설계시 주의점

--- jpa-springBoot/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-springBoot/src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -31,7 +32,7 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
     private LocalDateTime orderDate;

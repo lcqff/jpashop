7d8bc4bab08c7697d5ce7f3318fc419f13e2b8f9
lcqff <yeonnu82g@gmail.com>
2023-11-08 20:04:26 +0900

    섹션2: JPA와 DB 설정, 동작확인

---
+++ jpa-springBoot/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import jpabook.jpashop.model.Member;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember () {
+        Member member = new Member();
+        member.setUserName("memberA");
+        
+        Long saveid = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveid);
+        memberRepository.find(saveid);
+
+        assertEquals(findMember.getId(), member.getId());
+        assertEquals(findMember.getUserName(), member.getUserName());
+
+    }
+
+    @Test
+    void find() {
+    }
+}

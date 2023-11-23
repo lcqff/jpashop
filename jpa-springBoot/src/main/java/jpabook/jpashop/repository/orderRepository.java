8da51c5ca73b621091d4e30161a5653015447e14
lcqff <yeonnu82g@gmail.com>
2023-11-23 20:29:20 +0900

    섹션6: 주문 리포지토리 개발

---
+++ jpa-springBoot/src/main/java/jpabook/jpashop/repository/orderRepository.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class orderRepository {
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order find(Long orderId) {
+       return em.find(Order.class, orderId);
+    }
+}

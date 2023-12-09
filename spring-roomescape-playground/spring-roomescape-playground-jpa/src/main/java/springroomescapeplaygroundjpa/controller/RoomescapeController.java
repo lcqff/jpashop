1b605d05fe1670ab75d92aaca6b1808d3fe015af
lcqff <yeonnu82g@gmail.com>
2023-12-09 10:00:00 +0900

    Spring MVC: 1단계 홈화면

---
+++ spring-roomescape-playground/spring-roomescape-playground-jpa/src/main/java/springroomescapeplaygroundjpa/controller/RoomescapeController.java
@@ -0,0 +1,13 @@
+package springroomescapeplaygroundjpa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RoomescapeController {
+    @GetMapping("/")
+    public String loadAdminPage() {
+        return "home";
+    }
+}
+
